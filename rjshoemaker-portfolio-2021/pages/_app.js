fd37486a0b653609fe1e70002f71f9bbc66d13a0
RJ Shoemaker <rjshoemaker55@gmail.com>
2021-01-19 19:53:15 -0500

    index page startd, particles added

--- rjshoemaker-portfolio-2021/pages/_app.js
+++ rjshoemaker-portfolio-2021/pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    denim: 'rgb(73, 112, 145)',
+    salmon: 'rgb(244, 158, 145)',
+    spruce: 'rgb(108, 197, 169)',
+    seagrass: 'rgb(224, 233, 206)',
+    aqua: 'rgb(39, 164, 221)',
+    olive: 'rgb(206, 184, 98)',
+    lime: 'rgb(154, 189, 59)',
+    hotpink: 'rgb(235, 68, 140)'
+  }
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
